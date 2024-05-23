44087f184342919d4010dac3183a7300b92b97cf
G-B-Machado <machado.guilhermeb@gmail.com>
2024-05-23 00:57:53 -0300

    Creating function search_by_type

--- pokestatus.py
+++ pokestatus.py
@@ -16,9 +16,22 @@ def search_pokemon_by_id(dict, id):
     else:
         print("Pokemon not found")
 
+def search_pokemon_by_type(dict, type):
+    list_pokemons = list(dict.values())
+    list_type_pokemons = []
+    for pokemon in list_pokemons:
+        if pokemon[1] == type:
+            list_type_pokemons.append(pokemon)
+    print(f"There are {len(list_type_pokemons)} {type} type pokemons")  
+
+
 def main():
     pokemons = get_pokemons()
-    print(search_pokemon_by_id(pokemons, "347"))
+    print(f"We have {len(pokemons)} pokemons")
+    id = input("Please, insert a pokemon id: ")
+    pokemon_types = input("Input a pokemon type: ")
+    print(search_pokemon_by_id(pokemons, id))
+    search_pokemon_by_type(pokemons, pokemon_types)
     
 if __name__ == "__main__":
     main()
