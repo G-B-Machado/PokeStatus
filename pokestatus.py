d235ff4219603a78f66b1006bd6c092c833a72ef
G-B-Machado <machado.guilhermeb@gmail.com>
2024-05-24 01:32:34 -0300

    add 2 functions: get a list of pokemons by generation and get all the legendary pokemons

--- pokestatus.py
+++ pokestatus.py
@@ -24,14 +24,43 @@ def search_pokemon_by_type(dict, type):
             list_type_pokemons.append(pokemon)
     print(f"There are {len(list_type_pokemons)} {type} type pokemons")  
 
+def transform_dict_to_list(dict):
+    list_pokemons = list(dict.values())
+    return list_pokemons
+    
+def get_pokemons_by_generation(list, generation):
+    generation_list = []
+    for pokemon in list:
+        if pokemon[10] == generation:
+            generation_list.append(pokemon[0])
+    return generation_list
+
+def get_legendary_pokemons(list, legendary):
+    legendary_list = []
+    for pokemon in list:
+        if pokemon[11] == legendary:
+            legendary_list.append(pokemon[0])
+    return legendary_list
 
 def main():
+
     pokemons = get_pokemons()
+    pokemon_list = transform_dict_to_list(pokemons)
+    generation_list = []
+
     print(f"We have {len(pokemons)} pokemons")
+
     id = input("Please, insert a pokemon id: ")
-    pokemon_types = input("Input a pokemon type: ")
     print(search_pokemon_by_id(pokemons, id))
+
+    pokemon_types = input("Input a pokemon type: ")
     search_pokemon_by_type(pokemons, pokemon_types)
-    
+
+    generation = input("Input a generation: ")
+    generation_list = get_pokemons_by_generation(pokemon_list, generation)
+    print(generation_list)
+
+    print(get_legendary_pokemons(pokemon_list, "True"))
+
 if __name__ == "__main__":
     main()
